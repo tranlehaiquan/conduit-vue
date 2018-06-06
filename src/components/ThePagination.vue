3bee115ba6ac792b7f7ad554b4a98812fc349ac3
tranlehaiquan@gmail.com <tranlehaiquan@gmail.com>
2018-06-07 00:18:19 +0700

    update pagination algorithm

--- src/components/ThePagination.vue
+++ src/components/ThePagination.vue
@@ -2,13 +2,18 @@
   <nav>
     <ul class="pagination" v-if="pageCount > 1">
       <li
-        v-for="page in pageCount"
+        v-for="page in pagination(value, pageCount)"
         :key="page"
         class="page-item"
         :class="isActive(page) && 'active'"
         @click.prevent="changePage(page)"
       >
-        <a class="page-link" href>{{page}}</a>
+        <template v-if="page !== '...'">
+          <a class="page-link" href>{{page}}</a>
+        </template>
+        <template v-else>
+          <a class="page-link">...</a>
+        </template>
       </li>
     </ul>
   </nav>
@@ -29,15 +34,45 @@ export default {
   },
   methods: {
     changePage (page) {
+      if (page === '...') return
       this.$emit('change', page - 1)
     },
     isActive (page) {
       if (page === this.value + 1) return true
       return false
+    },
+    pagination (c, m) {
+      let current = c
+      let last = m
+      let delta = 2
+      let left = current - delta
+      let right = current + delta + 1
+      let range = []
+      let rangeWithDots = []
+      let l
+
+      for (let i = 1; i <= last; i++) {
+        if (i === 1 || i === last || (i >= left && i < right)) {
+          range.push(i)
+        }
+      }
+
+      for (let i of range) {
+        if (l) {
+          if (i - l === 2) {
+            rangeWithDots.push(l + 1)
+          } else if (i - l !== 1) {
+            rangeWithDots.push('...')
+          }
+        }
+        rangeWithDots.push(i)
+        l = i
+      }
+
+      return rangeWithDots
     }
   },
   computed: {
-
   }
 }
 </script>
